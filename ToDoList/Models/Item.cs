8e3fa8a22b196bd21375d89d940722afe8391fad
Leilani Leach <leilanileach@yahoo.com>
2018-11-27 12:23:46 -0800

    add delete item functionality

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -2,36 +2,35 @@ using System.Collections.Generic;
 
 namespace ToDoList.Models
 {
-  public class Item
-  {
-    private string _description;
-    private static List<Item> _instances = new List<Item> {};
-
-    public Item (string description)
+    public class Item
     {
-      _description = description;
-      _instances.Add(this);
-    }
+        private string _description;
+        private static List<Item> _instances = new List<Item> { };
 
-    public string GetDescription()
-    {
-      return _description;
-    }
+        public Item(string description)
+        {
+            _description = description;
+        }
+        public string GetDescription()
+        {
+            return _description;
+        }
+        public void SetDescription(string newDescription)
+        {
+            _description = newDescription;
+        }
+        public static List<Item> GetAll()
+        {
+            return _instances;
+        }
+        public void Save()
+        {
+            _instances.Add(this);
+        }
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
 
-    public void SetDescription(string newDescription)
-    {
-      _description = newDescription;
     }
-
-    public static List<Item> GetAll()
-    {
-      return _instances;
-    }
-
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
-
-  }
 }
